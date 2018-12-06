78bfb02f89fca39b5cdecb825760c7354974b058
erucolindo96 <erucolindo96@gmail.com>
2018-12-06 22:36:01 +0100

    Dodano ConfigManager, fasadę dla alize::Config, umożliwiającą zapis/odczyt tylko wybranych parametrów.

---
+++ configuration/ConfigManager.cpp
@@ -0,0 +1,60 @@
+﻿#include "ConfigManager.hpp"
+
+ConfigManager::ConfigManager(ConfigManager &&other):alize::Config(other)
+{}
+
+ConfigManager& ConfigManager::operator=(ConfigManager &&other)
+{
+  if(this == &other)
+    return *this;
+  alize::Config::operator =(other);
+  return *this;
+}
+
+uint32_t ConfigManager::getVectSize()const
+{
+  return getParam_vectSize();
+}
+void ConfigManager::setVectSize(uint32_t vect_size)
+{
+  setParam(PARAM_VECT_SIZE, std::to_string(vect_size).c_str());
+}
+
+std::string ConfigManager::getModelFolder()const
+{
+  return getParam(PARAM_MODEL_DIR).c_str();
+}
+void ConfigManager::setModelFolder(std::string folder)
+{
+  setParam(PARAM_MODEL_DIR, folder.c_str());
+}
+
+std::string ConfigManager::getUbmFolder()const
+{
+  return getParam(PARAM_UBM_DIR).c_str();
+}
+
+void ConfigManager::setUbmFolder(std::string folder)
+{
+  setParam(PARAM_UBM_DIR, folder.c_str());
+}
+
+std::string ConfigManager::getFeatureFolder()const
+{
+  return getParam(PARAM_FEATURE_FOLDER).c_str();
+}
+
+void ConfigManager::setFeatureFolder(std::string folder)
+{
+  setParam(PARAM_FEATURE_FOLDER, folder.c_str());
+}
+
+void ConfigManager::load(const alize::FileName &f)
+{
+  alize::Config::load(f);
+}
+
+void ConfigManager::save(const alize::FileName &f) const
+{
+  alize::Config::save(f);
+}
---
+++ configuration/ConfigManager.hpp
@@ -0,0 +1,35 @@
+﻿#ifndef CONFIGMANAGER_HPP
+#define CONFIGMANAGER_HPP
+#include<alize.h>
+
+class ConfigManager: protected alize::Config
+{
+
+public:
+  const alize::String PARAM_UBM_DIR = "ubmModelDir", PARAM_MODEL_DIR = "modelDir",
+  PARAM_VECT_SIZE = "vectSize", PARAM_FEATURE_FOLDER = "featureFolder";
+  ConfigManager()=default;
+  ConfigManager(const ConfigManager &other)=default;
+  ConfigManager& operator=(const ConfigManager &other)=default;
+  ConfigManager(ConfigManager &&other);
+  ConfigManager& operator=(ConfigManager &&other);
+
+  uint32_t getVectSize()const;
+  void setVectSize(uint32_t vect_size);
+
+  std::string getModelFolder()const;
+  void setModelFolder(std::string folder);
+
+  std::string getUbmFolder()const;
+  void setUbmFolder(std::string folder);
+
+  std::string getFeatureFolder()const;
+  void setFeatureFolder(std::string folder);
+
+  void load(const alize::FileName &f);
+  void save(const alize::FileName &f) const;
+
+  virtual ~ConfigManager()=default;
+};
+
+#endif // CONFIGMANAGER_HPP
