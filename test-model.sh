5f4965bf7ea3e21d1f37378958987fc5db04b7c8
erucolindo96 <erucolindo96@gmail.com>
2019-01-21 00:02:02 +0100

    Zmodyfikowano skrypt testujący weryfikacje modeli
    1) Próbki niezgodne brane są z wszystkich innych modeli, nie tylko z następnego w tablicy
    2) Funkcja weryfikująca uruchamiana jest dla różnych wartości progu (od 0.1 do 1)

--- test-model.sh
+++ test-model.sh
@@ -9,59 +9,95 @@ function recognition()
   local sample_dir=$3
   local threshold=$4
 
-  echo $model1
-  echo $modelUbm
-  echo $sample_dir
-  echo $threshold
+#  echo $model1
+#  echo $modelUbm
+#  echo $sample_dir
+#  echo $threshold
+
+  local succes_sum=0
 
   for file in $sample_dir/*
   do
     echo $file
 
-    ./main -r models/ $model1 $modelUbm $threshold $file 
+    ./main -r models/ $model1 $modelUbm $threshold $file
+    succes_sum=$(($succes_sum+$?))
     echo "----------------------------------------------" 
     echo "----------------------------------------------" 	
 	
   done
+  echo "pozytywne weryfikacje: $succes_sum"   
+  return $succes_sum     
 }
 
 
+function test_models()
+{
 
-prog=$1	
+local prog=$1	
+local ubm=$2
 
-models=("ferus-bez-ciszy" "janFigat-bez-ciszy" "kasprzak-bez-ciszy" "maksymFigat-bez-ciszy") 
+local models=("ferus-bez-ciszy" "janFigat-bez-ciszy" "kasprzak-bez-ciszy" "maksymFigat-bez-ciszy") 
 
-model_dirs=("probki/ferus/rozpoznawanie" "probki/janFigat/rozpoznawanie" "probki/kasprzak/rozpoznawanie" "probki/maksymFigat/rozpoznawanie")
+local model_dirs=("probki/ferus/rozpoznawanie" "probki/janFigat/rozpoznawanie" "probki/kasprzak/rozpoznawanie" "probki/maksymFigat/rozpoznawanie")
 
-ubms=("ubm-bez-ciszy-bez-rozpoznawania" "ubm-bez-ciszy-bez-uczacych-rozpoznawanych")
+#ubms=("ubm-bez-ciszy-bez-rozpoznawania" "ubm-bez-ciszy-bez-uczacych-rozpoznawanych")
 
-models_zgodne=(0 0 0 0)
-models_niezgodne=(0 0 0 0)
+local sample_cnt=20
 
-ferus=0
-jan=1
-kasprzak=2
-maksym=3
-models_cnt=4
+local models_zgodne=(0 0 0 0)
+local models_niezgodne=($sample_cnt $sample_cnt $sample_cnt $sample_cnt)
 
+#local ferus=0
+#local jan=1
+#local kasprzak=2
+#local maksym=3
+local models_cnt=4
 
-for ubm in ${ubms[0]} ${ubms[1]} 
-do
-
-  echo "UBM równy $ubm  "
-  for i in {0..3}
+for i in {0..3}
   do
     echo "Próbki zgodne"
     recognition ${models[i]} $ubm ${model_dirs[i]} $prog
-    echo "Próbki niezgodne"
-	recognition ${models[i]} $ubm ${model_dirs[(i+1)%$models_cnt]} $prog
-	echo "#############################################################"
-	echo "#############################################################"
+    models_zgodne[$i]=$(($?))
+	echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"    
+	echo "Próbki niezgodne"
 
- 
-  done
-  echo "*****************************************************"
-  echo "*****************************************************"
+	for j in {1..3} #robimy weryfikacje dla wszystkich folderów próbek poza próbkami naszego modelu(test próbek niezgodnych)
+	do
+	  recognition ${models[i]} $ubm ${model_dirs[(i+j)%$models_cnt]} $prog
+	  models_niezgodne[$i]=$((${models_niezgodne[$i]}+$?))
+	  echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+	done    
+
+	echo "#############################################################" 
+done
+
+echo "*****************************************************"
+
+
+echo "Prób rozpoznawania wynosił: $prog" >> test-model-result.txt
+echo "Używany model UBM: $ubm" >> test-model-result.txt
+echo "Weryfikacji dla próbek zgodnych jest $sample_cnt" >> test-model-result.txt
+echo "Zaś dla niezgodnych jest $(($sample_cnt*3))" >> test-model-result.txt
+
+for i in {0..3}
+  do
+    echo "Model ${models[$i]} uzyskał ${models_zgodne[$i]} pozytywnych weryfikacji dla próbek zgodnych" >> test-model-result.txt
+    echo "oraz ${models_niezgodne[$i]} pozytywnych weryfikacji dla próbek niezgodnych" >> test-model-result.txt
+    echo "próbki niezgodne należą do modelu ${models[(i+1)%$models_cnt]}" >> test-model-result.txt
+done
+
+}
+
+#base_thr=0.1
+ubm=$1
+
+for thr in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1
+do
+
+echo "Próg == $thr"
+test_models $thr $ubm 
+
 done
 
 
