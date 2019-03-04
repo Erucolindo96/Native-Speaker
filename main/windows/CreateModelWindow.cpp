9ad79cdbe245b80277b3dbde4e65b5c239ecca6c
erucolindo96 <erucolindo96@gmail.com>
2019-03-04 14:39:14 +0100

    Dodano walidację danych pobieranych od usera przy tworzeniu modelu.
    Warto dopisać testy klasy Walidującej. Może również warto przenieść ją do osobnego pliku

--- main/windows/CreateModelWindow.cpp
+++ main/windows/CreateModelWindow.cpp
@@ -5,7 +5,6 @@ CreateModelWindow::CreateModelWindow(QWidget *parent) :
 {
   ui.setupUi(this);
   connect(ui.cancell_button, SIGNAL (clicked()), this, SLOT(close()));
-
 }
 
 
@@ -33,20 +32,24 @@ ConfigManager CreateModelWindow::getConfig()const
 void CreateModelWindow::on_create_button_released()
 {
   //TODO - zrobic walidacje
-  auto model_name = ui.textEdit_model_name->document()->firstBlock().text();
-  cout<<model_name.toStdString()<<endl;
-  auto distrib_cnt = ui.textEdit_distrib_cnt->document()->firstBlock().text().toInt();
-  cout<<distrib_cnt<<endl;
-  auto description = ui.textEdit_description->document()->firstBlock().text();
-  const std::map<string, alize::DistribType> distrib_type_mapping
+  const std::map<string, alize::DistribType> DISTRIB_TYPE_MAP
   {
     {ui.comboBox_distrib_type->itemText(0).toStdString(), alize::DistribType_GD},
     {ui.comboBox_distrib_type->itemText(1).toStdString(), alize::DistribType_GF}
   };
-  alize::DistribType type = distrib_type_mapping.at
+  auto model_name = ui.textEdit_model_name->document()->firstBlock().text();
+  auto distrib_cnt_str = ui.textEdit_distrib_cnt->document()->firstBlock().text();
+  auto description = ui.textEdit_description->document()->firstBlock().text();
+  alize::DistribType type = DISTRIB_TYPE_MAP.at
                             (ui.comboBox_distrib_type->currentText().toStdString());
-  cout<<type<<endl;
-  createModel(model_name, type, description, distrib_cnt, config_.getVectSize());
+  if(!reactOnInvalidModelName(model_name) ||
+     !reactOnInvalidDescription(description) ||
+     !reactOnInvalidDistribCnt(distrib_cnt_str))
+  {
+    return;
+  }
+
+  createModel(model_name, type, description, distrib_cnt_str.toInt(), config_.getVectSize());
   emit accepted();
   close();
 }
@@ -66,3 +69,101 @@ void CreateModelWindow::createModel(QString model_name,alize::DistribType type,
   }
 
 }
+
+bool CreateModelWindow::reactOnInvalidModelName(const QString &model_name)const
+{
+  if(!validator_.isModelValid(model_name))
+  {
+    QString msg = QString("Model name is invalid. "
+                                "Please remove from model name spaces and "
+                                "characters: ");
+    foreach (auto character, validator_.invalid_chars_model_name_)
+    {
+      msg.append(character);
+      msg.append(", ");
+    }
+    msg.append(".");
+    QMessageBox::warning(const_cast<CreateModelWindow*>(this),
+                         "Invalid Data",
+                         msg ,QMessageBox::Ok);
+    return false;
+  }
+  return true;
+}
+
+bool CreateModelWindow::reactOnInvalidDescription(const QString  &desc)const
+{
+  if(!validator_.isDescriptionValid(desc))
+  {
+    QString msg = QString("Description is invalid. Please correct them.");
+    QMessageBox::warning(const_cast<CreateModelWindow*>(this),
+                       "Invalid Data",
+                       msg ,QMessageBox::Ok);
+    return false;
+  }
+  return true;
+}
+
+bool CreateModelWindow::reactOnInvalidDistribCnt(const QString &distrib_cnt)const
+{
+  if(!validator_.isDistribCntValid(distrib_cnt))
+  {
+    QString msg = QString("Distrib count is invalid. It must be non-zero positive number.");
+    QMessageBox::warning(const_cast<CreateModelWindow*>(this),
+                       "Invalid Data",
+                       msg ,QMessageBox::Ok);
+    return false;
+  }
+  return true;
+}
+
+
+
+
+
+
+bool CreateModelWindow::CreateModelValidator::isModelValid(const QString &model_name)const
+{
+  bool ret = true;
+  if(model_name.isEmpty())
+  {
+    ret = false;
+  }
+  foreach (QChar elem, invalid_chars_model_name_)
+  {
+    if(model_name.contains(elem))
+    {
+      ret = false;
+    }
+  }
+  return ret;
+}
+bool CreateModelWindow::CreateModelValidator::isDescriptionValid(const QString &desc)const
+{
+  return true;
+}
+bool CreateModelWindow::CreateModelValidator::isDistribCntValid(const QString &distrib_cnt)const
+{
+  bool is_int = false;
+  int distrib_cnt_int = distrib_cnt.toInt(&is_int);
+  if(!is_int || distrib_cnt_int <= 0)
+  {
+    return false;
+  }
+  return true;
+}
+/*
+bool CreateModelWindow::CreateModelValidator::isTypeValid(const QString &type,
+                                                          std::map<string, alize::DistribType> &mapping)const
+{
+  try
+  {
+      alize::DistribType conv_type = mapping.at(type.toStdString());
+  }
+  catch(std::out_of_range e)
+  {
+    return false;
+  }
+  return true;
+}
+*/
--- main/windows/CreateModelWindow.hpp
+++ main/windows/CreateModelWindow.hpp
@@ -9,11 +9,32 @@
 #include<map>
 #include<alize.h>
 #include<configuration/ConfigManager.hpp>
+#include<QMessageBox>
+
 class CreateModelWindow : public QDialog//, public Runnable
 {
   Q_OBJECT
 
 public:
+  class CreateModelValidator
+    {
+  public:
+      const QList<QChar> invalid_chars_model_name_ = {'/', '\\', '\'', '\'', ' ', '\t'};
+
+      explicit CreateModelValidator() = default;
+      CreateModelValidator(const CreateModelValidator& other) = default;
+      CreateModelValidator& operator =(const CreateModelValidator &other) = default;
+      CreateModelValidator(CreateModelValidator&& other) = default;
+      CreateModelValidator& operator =(CreateModelValidator &&other) = default;
+
+
+      bool isModelValid(const QString &model_name)const;
+      bool isDescriptionValid(const QString &desc)const;
+      bool isDistribCntValid(const QString &distrib_cnt)const;
+    // brak walidacji typu, jeśli wystąpi wyjątek to znaczy, ze cos jest nie tak w kodzie -
+    //taka sytuacja nie powinna wystąpic, bo tam mamy liste z której user wybiera elementy
+    //bool isTypeValid(const QString &type, std::map<string, alize::DistribType> &mapping)const;
+    };
   explicit CreateModelWindow(QWidget *parent = 0);
   std::unique_ptr<GmmModel> getCreatedGmmModel();
   void setConfig(ConfigManager conf);
@@ -34,8 +55,17 @@ private:
   Ui::CreateModelWindow ui;
   std::unique_ptr<GmmModel> created_model_;
   ConfigManager config_;
+  CreateModelValidator validator_;
+
   void createModel(QString model_name,alize::DistribType type,
                    QString description, uint32_t distrib_cnt, uint32_t vect_size);
+  bool reactOnInvalidModelName(const QString &model_name)const;
+  bool reactOnInvalidDescription(const QString  &desc)const;
+  bool reactOnInvalidDistribCnt(const QString &distrib_cnt)const;
+  // brak walidacji typu, jeśli wystąpi wyjątek to znaczy, ze cos jest nie tak w kodzie -
+  //taka sytuacja nie powinna wystąpic, bo tam mamy liste z której user wybiera elementy
+//  bool reactOnInvalidType(const QString &distrib_cnt);
+
 
 };
 
